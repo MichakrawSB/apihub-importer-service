b0ff477aecb99496ec6a4396c779498d55c102f4
michal.krawczyk <michal.krawczyk@smartbear.com>
2025-04-07 17:02:39 +0200

    build: initial commit (ACT-3226)

---
+++ importer-service/src/test/java/com/smartbear/api/importerservice/ImporterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbear.api.importerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImporterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
